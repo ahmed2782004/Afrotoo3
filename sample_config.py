608452fef6e906a5b2bf2b4c64c2e15ccd92cc7e
ahmed2782004 <147652645+ahmed2782004@users.noreply.github.com>
2023-10-31 01:54:03 +0200

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -28,7 +28,7 @@ class Config(object):
     TZ = os.environ.get("TZ", "Asia/Baghdad")
     # set this with required cat repo link
     UPSTREAM_REPO = os.environ.get(
-        "UPSTREAM_REPO", "https://github.com/jepthoniq/jepthon.git"
+        "UPSTREAM_REPO", "https://github.com/ahmed2782004/Afrotoo3.git"
     )
 
     # BASIC and MAIN CONFIG VARS
